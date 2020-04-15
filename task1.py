0afa45b0f8d67c6f52bedc5aa7cc45d89c61060a
Anders Hoel <andershoel@HoelBook-Pro.local>
2020-04-15 09:43:34 +0200

    Made file 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+import numpy.linalg as npl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+from scipy.sparse import identity, dok_matrix
+from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import factorized
